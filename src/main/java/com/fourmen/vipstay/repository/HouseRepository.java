0951bb7c736ac2038e7d6c58fba60426e0a36f52
KhanhNguyen206 <nguyen.ngockhanh206@gmail.com>
2019-08-20 17:12:38 +0700

    add House Repository

---
+++ src/main/java/com/fourmen/vipstay/repository/HouseRepository.java
@@ -0,0 +1,7 @@
+package com.fourmen.vipstay.repository;
+
+import com.fourmen.vipstay.model.House;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HouseRepository extends JpaRepository<House, Long> {
+}
