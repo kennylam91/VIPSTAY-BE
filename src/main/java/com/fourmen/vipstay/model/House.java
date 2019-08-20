1024e009102c24e7ce9290af062630f41c639f75
KhanhNguyen206 <nguyen.ngockhanh206@gmail.com>
2019-08-20 17:08:57 +0700

    add model House

---
+++ src/main/java/com/fourmen/vipstay/model/House.java
@@ -0,0 +1,125 @@
+package com.fourmen.vipstay.model;
+
+import javax.persistence.*;
+
+@Entity
+public class House {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String houseName;
+
+    private String typeName;
+
+    private String address;
+
+    private Long bedRoomNumber;
+
+    private Long bathroomNumber;
+
+    @Column(columnDefinition = "long")
+    private String description;
+
+    private Long price;
+
+    private String image;
+
+    private Long rate;
+
+    public House() {
+    }
+
+    public House(String houseName, String typeName, String address, Long bedRoomNumber, Long bathroomNumber, String description, Long price, String image, Long rate) {
+        this.houseName = houseName;
+        this.typeName = typeName;
+        this.address = address;
+        this.bedRoomNumber = bedRoomNumber;
+        this.bathroomNumber = bathroomNumber;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.rate = rate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHouseName() {
+        return houseName;
+    }
+
+    public void setHouseName(String houseName) {
+        this.houseName = houseName;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getBedRoomNumber() {
+        return bedRoomNumber;
+    }
+
+    public void setBedRoomNumber(Long bedRoomNumber) {
+        this.bedRoomNumber = bedRoomNumber;
+    }
+
+    public Long getBathroomNumber() {
+        return bathroomNumber;
+    }
+
+    public void setBathroomNumber(Long bathroomNumber) {
+        this.bathroomNumber = bathroomNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Long getRate() {
+        return rate;
+    }
+
+    public void setRate(Long rate) {
+        this.rate = rate;
+    }
+}
