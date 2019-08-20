24b883c694b183146b7ed2abd65f881f5f8c9559
kennylam91 <phamvanlam1991@gmail.com>
2019-08-20 14:01:16 +0700

    start project

---
+++ src/main/java/com/fourmen/vipstay/VipstayApplication.java
@@ -0,0 +1,13 @@
+package com.fourmen.vipstay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipstayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VipstayApplication.class, args);
+    }
+
+}
