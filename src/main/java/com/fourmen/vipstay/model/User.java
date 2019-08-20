aa2bf285c94fe71f1a0d8783df0ed0b9686f29ff
elnino228 <daodat228@gmail.com>
2019-08-20 14:45:20 +0700

    add properties of user

--- src/main/java/com/fourmen/vipstay/model/User.java
+++ src/main/java/com/fourmen/vipstay/model/User.java
@@ -1,4 +1,15 @@
 package com.fourmen.vipstay.model;
 
 public class User {
+    String username;
+    String password;
+    int id;
+    String name;
+    int age;
+    String type;
+    String address;
+    int phone;
+    String email;
+    String idNumber;
+    String avatar;
 }
