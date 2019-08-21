b0340335b4cfbce011b2765342e8213553f5f7ed
kennylam91 <phamvanlam1991@gmail.com>
2019-08-21 11:50:24 +0700

    add some changes to get the war file after build

--- src/main/java/com/fourmen/vipstay/VipstayApplication.java
+++ src/main/java/com/fourmen/vipstay/VipstayApplication.java
@@ -4,18 +4,24 @@ import com.fourmen.vipstay.service.HouseService;
 import com.fourmen.vipstay.service.impl.HouseServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class VipstayApplication {
+public class VipstayApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VipstayApplication.class, args);
     }
 
     @Bean
-    public HouseService houseService(){
+    public HouseService houseService() {
         return new HouseServiceImpl();
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(VipstayApplication.class);
+    }
 }
